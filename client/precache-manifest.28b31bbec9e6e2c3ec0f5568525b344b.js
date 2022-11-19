cf97347c47ee351e9d4cb456f0698c538cf31df0
arshdeep2606 <68486945+arshdeep2606@users.noreply.github.com>
2022-11-19 11:45:50 -0500

    #1

---
+++ client/precache-manifest.28b31bbec9e6e2c3ec0f5568525b344b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406f953e5dfcdd68ab24d3bf06940ea5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14d35cce41627b0d26f0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7fad80d1febe77bb7bfa",
+    "url": "/static/css/main.1c577351.chunk.css"
+  },
+  {
+    "revision": "14d35cce41627b0d26f0",
+    "url": "/static/js/2.3d862d9f.chunk.js"
+  },
+  {
+    "revision": "e828f7f811b0551b1ca97c07a9f864c6",
+    "url": "/static/js/2.3d862d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fad80d1febe77bb7bfa",
+    "url": "/static/js/main.cb43916a.chunk.js"
+  },
+  {
+    "revision": "e15ed4054158b0a072ca",
+    "url": "/static/js/runtime-main.1a03e15a.js"
+  },
+  {
+    "revision": "6641d2b3f850d9c935e5937a2b9de8ac",
+    "url": "/static/media/blog.6641d2b3.png"
+  }
+]);
